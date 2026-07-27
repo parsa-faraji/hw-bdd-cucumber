bd6073c51aec1b24e61f8c1ef45a43aa01e295cc
Parsa Faraji Alamouti <parsafaraji@berkeley.edu>
2026-07-27 04:15:22 +0000

    Implement declarative movie database setup step

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,12 +1,9 @@
-# Add a declarative step here for populating the DB with movies.
-
 Given(/the following movies exist/) do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  pending "Fill in this step in movie_steps.rb"
-end
+end# Add a declarative step here for populating the DB with movies.
+
 
 Then(/(.*) seed movies should exist/) do |n_seeds|
   expect(Movie.count).to eq n_seeds.to_i
